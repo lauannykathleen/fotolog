03cf1edffc6770d772b528d087e962b03a0e50ad
Jorge Marinho <jorgeluizbsi@gmail.com>
2022-04-08 17:10:09 -0300

    melhorando

--- public/index.php
+++ public/index.php
@@ -1,10 +1,9 @@
 <?php
 
-require '../config/config.php';
-include '../app/controller/IndexController.php';
+require_once '../config/config.php';
+require_once '../vendor/autoload.php';
 
 use App\Controller\IndexController;
-
 
 $objController = new IndexController($_GET['a']);
 $objController->iniciar();
